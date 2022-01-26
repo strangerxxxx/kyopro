be82fae3ea4fe6c328732700461e7e13d582c084
strangerxxxx <ogawa_first@yahoo.co.jp>
2022-01-26 18:51:52 +0900

    Update template.cbl

--- cobol/template.cbl
+++ cobol/template.cbl
@@ -7,41 +7,26 @@
        DATA DIVISION.
        FILE SECTION.
            FD SYSIN.
-               01 INDATA PIC X(2000000).
+               01 INP PIC X(2000000).
        WORKING-STORAGE SECTION.
            01 WK.
-               03 INP PIC X(2000000).
                03 I PIC 9(18).
                03 J PIC 9(18).
+               03 K PIC 9(18).
                03 N PIC 9(18).
-               03 ANS PIC 9(17).
-               03 ANS-Z PIC Z(14)9.
+               03 M PIC 9(18).
+               03 ANS PIC 9(18).
+               03 ANS-Z PIC Z(17)9.
        PROCEDURE DIVISION.
            ACCEPT N.
            OPEN INPUT SYSIN.
-           READ SYSIN INTO INP.
+           READ SYSIN.
            CLOSE SYSIN.
-           
+           UNSTRING INP DELIMITED BY SPACE INTO N M.
            MOVE ANS TO ANS-Z.
            DISPLAY FUNCTION TRIM(ANS-Z).
            STOP RUN.
        END PROGRAM ATCODER.
-
-      *    SPLIT LIST
-       INPUT-OUTPUT SECTION.
-       FILE-CONTROL.
-           SELECT SYSIN ASSIGN TO KEYBOARD ORGANIZATION LINE SEQUENTIAL.
-
-       FILE SECTION.
-           FD SYSIN.
-               01 INDATA PIC X(2000000).
-
-               03 I PIC 9(18).
-               03 PT PIC 9(18).
-
-           OPEN INPUT SYSIN.
-           READ SYSIN INTO INP.
-           CLOSE SYSIN.
 
       *    SPLIT
            UNSTRING INP DELIMITED BY SPACE INTO N M.
@@ -52,16 +37,19 @@
                03 PT PIC 9(18) VALUE 1.
            01 AL.
                03 A OCCURS 10000 TIMES PIC 9(5).
-
+       PROCEDURE DIVISION.
+           OPEN INPUT SYSIN.
+           READ SYSIN.
            MOVE 1 TO PT.
            PERFORM VARYING I FROM 1 BY 1 UNTIL I > N
                UNSTRING INP DELIMITED BY SPACE INTO A(I) WITH POINTER PT
            END-PERFORM.
+           CLOSE SYSIN.
 
       *    SORT
            01 AL.
                03 AI OCCURS 1 TO 100000 TIMES DEPENDING ON N.
-                   05 A PIC 9(5).
+                   05 A PIC 9(10).
            SORT AI ON DESCENDING KEY A.
            SORT AI ON ASCENDING KEY A.
 
