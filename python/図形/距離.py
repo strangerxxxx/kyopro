ab03e91de6aeee3b95fb43164957bc96354f67d3
strangerxxxx <ogawa_first@yahoo.co.jp>
2022-03-01 16:34:48 +0900

    Update 距離.py

--- python/図形/距離.py
+++ python/図形/距離.py
@@ -1,18 +1,18 @@
-def distance(a: int, b: int) -> float:
-    # 2点間のユークリッド距離(普通の距離)
+def distance(a, b) -> float:
+    # 2点間のユークリッド距離
     d = 0
     for i, j in zip(a, b):
         d += (i - j) ** 2
     return d ** 0.5
 
 
-def euclidean_scipy(a: int, b: int) -> float:
-    # 2点間のユークリッド距離(普通の距離)
+def euclidean_scipy(a, b) -> float:
+    # 2点間のユークリッド距離
     from scipy.spatial.distance import euclidean
     return euclidean(a, b)
 
 
-def manhattan_distance(a: int, b: int) -> int:
+def manhattan_distance(a, b) -> int:
     # 2点間のマンハッタン距離
     d = 0
     for i, j in zip(a, b):
@@ -20,13 +20,13 @@ def manhattan_distance(a: int, b: int) -> int:
     return d
 
 
-def manhattan_scipy(a: int, b: int) -> int:
+def manhattan_scipy(a, b) -> int:
     # 2点間のマンハッタン距離(Scipy)
     from scipy.spatial.distance import cityblock
     return cityblock(a, b)
 
 
-def euclidean_numpy(a: int, b: int) -> float:
+def euclidean_numpy(a, b) -> float:
     # Numpy編 np.arrayがinput マンハッタン距離はord=1
     from numpy.linalg import norm
     return norm(a - b)
