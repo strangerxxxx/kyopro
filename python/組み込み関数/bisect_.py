2a2c625d35f918dae38e5fd83caa14e1392abe0d
strangerxxxx <ogawa_first@yahoo.co.jp>
2022-03-01 12:05:01 +0900

    Update bisect_.py

--- python/組み込み関数/bisect_.py
+++ python/組み込み関数/bisect_.py
@@ -1,80 +1,93 @@
-def index_left(a, x):
+def index_left(a, x: int) -> int:
     'Locate the leftmost value exactly equal to x'
-    import bisect
-    i = bisect.bisect_left(a, x)
+    from bisect import bisect_left
+    i = bisect_left(a, x)
     if i != len(a) and a[i] == x:
         return i
     raise ValueError
 
 
-def index_right(a, x):
+def index_right(a, x: int) -> int:
     'Locate the rightmost value exactly equal to x'
-    import bisect
-    i = bisect.bisect_right(a, x) - 1
+    from bisect import bisect_right
+    i = bisect_right(a, x)
     if i != len(a) and a[i] == x:
         return i
     raise ValueError
 
 
-def find_lt(a, x):
+def find_lt(a, x: int) -> int:
     'Find rightmost value less than x'
-    import bisect
-    i = bisect.bisect_left(a, x)
+    from bisect import bisect_left
+    i = bisect_left(a, x)
     if i:
         return a[i - 1]
     raise ValueError
 
 
-def find_le(a, x):
+def find_le(a, x: int) -> int:
     'Find rightmost value less than or equal to x'
-    import bisect
-    i = bisect.bisect_right(a, x)
+    from bisect import bisect_right
+    i = bisect_right(a, x)
     if i:
         return a[i - 1]
     raise ValueError
 
 
-def find_gt(a, x):
+def find_gt(a, x: int) -> int:
     'Find leftmost value greater than x'
-    import bisect
-    i = bisect.bisect_right(a, x)
+    from bisect import bisect_right
+    i = bisect_right(a, x)
     if i != len(a):
         return a[i]
     raise ValueError
 
 
-def find_ge(a, x):
+def find_ge(a, x: int) -> int:
     'Find leftmost item greater than or equal to x'
-    import bisect
-    i = bisect.bisect_left(a, x)
+    from bisect import bisect_left
+    i = bisect_left(a, x)
     if i != len(a):
         return a[i]
     raise ValueError
 
 
-def count_lt(a, x):
+def find_bothsides(a, x: int) -> tuple:
+    from bisect import bisect_left
+    i = bisect_left(a, x)
+    if i:
+        if i < len(a):
+            return a[i - 1] + 1, a[i] - 1
+        else:
+            return a[i - 1] + 1, None
+    if a:
+        return None, a[0] - 1
+    return None, None
+
+
+def count_lt(a, x: int) -> int:
     'Count value less than x'
-    import bisect
-    i = bisect.bisect_left(a, x)
+    from bisect import bisect_left
+    i = bisect_left(a, x)
     return i
 
 
-def count_le(a, x):
+def count_le(a, x: int) -> int:
     'Count value less than or equal to x'
-    import bisect
-    i = bisect.bisect_right(a, x)
+    from bisect import bisect_right
+    i = bisect_right(a, x)
     return i
 
 
-def count_gt(a, x):
+def count_gt(a, x: int) -> int:
     'Count value greater than x'
-    import bisect
-    i = bisect.bisect_right(a, x)
+    from bisect import bisect_right
+    i = bisect_right(a, x)
     return len(a) - i
 
 
-def count_ge(a, x):
+def count_ge(a, x: int) -> int:
     'Count item greater than or equal to x'
-    import bisect
-    i = bisect.bisect_left(a, x)
+    from bisect import bisect_left
+    i = bisect_left(a, x)
     return len(a) - i
