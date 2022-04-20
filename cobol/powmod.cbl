68498ead8729ab49d327ade5904c34f94455d0e9
strangerxxxx <ogawa_first@yahoo.co.jp>
2022-04-20 11:17:25 +0900

    Create powmod.cbl

---
+++ cobol/powmod.cbl
@@ -0,0 +1,24 @@
+
+           01 POW-WK.
+               03 POW-A BINARY-DOUBLE SIGNED VALUE 0.
+               03 POW-N BINARY-DOUBLE SIGNED VALUE 0.
+               03 POW-MOD BINARY-DOUBLE SIGNED VALUE 998244353.
+               03 POW-RES BINARY-DOUBLE SIGNED VALUE 0.
+               03 POW-X BINARY-DOUBLE SIGNED VALUE 0.
+
+
+      *>   COMPUTE POW-A ** POW-N MOD POW-MOD
+       POWMOD SECTION.
+           MOVE 1 TO POW-RES
+           PERFORM UNTIL POW-N <= 0
+               MOVE 1 TO POW-X
+               COMPUTE POW-X = POW-N / 2
+               IF POW-N - 2 * POW-X = 1
+                   MULTIPLY POW-A BY POW-RES
+                   COMPUTE POW-RES = FUNCTION MOD(POW-RES, POW-MOD)
+               END-IF
+               MULTIPLY POW-A BY POW-A
+               COMPUTE POW-A = FUNCTION MOD(POW-A, POW-MOD)
+               DIVIDE 2 INTO POW-N
+           END-PERFORM
+       EXIT SECTION.
