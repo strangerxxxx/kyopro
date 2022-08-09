5e3a9e4f968ccaad01ebc89d28ca72d1915bf189
strangerxxxx <ogawa_first@yahoo.co.jp>
2022-08-09 13:47:29 +0900

    Create 二分探索.cpp

---
+++ c++/二分探索.cpp
@@ -0,0 +1,28 @@
+#ifdef STRANGERXXX
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+typedef long long ll;
+bool f(ll mid)
+{
+  return true;
+}
+int main()
+{
+  ll ok = 0;
+  ll ng = 1E18;
+  ll mid;
+  while (abs(ok - ng) > 1)
+  {
+    mid = (ok + ng) / 2;
+    if (f(mid))
+    {
+      ok = mid;
+    }
+    else
+    {
+      ng = mid;
+    }
+  }
+  print(ok);
+}
