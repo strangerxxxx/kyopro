e8d9355919ca6d9798aa1854366ff52623f1a82f
strangerxxxx <ogawa_first@yahoo.co.jp>
2022-08-24 14:28:36 +0900

    Update オイラーツアー.py

--- python/木/オイラーツアー.py
+++ python/木/オイラーツアー.py
@@ -18,6 +18,25 @@ def EulerTour(n, edges, start=0):
     return res
 
 
+def TopoSort(n, edges, start=0):
+    # 行きがけの順と親を含まない木を返す
+    parent = [None] * n
+    queue = [start]
+    tree = [[] for _ in range(n)]
+    order = []
+    parent[start] = -1
+    while queue:
+        i = queue.pop()
+        order.append(i)
+        # for a in reversed(sorted(edges[i])):
+        for a in edges[i]:
+            if parent[a] is None:
+                tree[i].append(a)
+                queue.append(a)
+                parent[a] = i
+    return order, tree, parent
+
+
 def EulerTour2(n, edges, start=0):
     reached = [False] * n
     queue = [~start, start]
