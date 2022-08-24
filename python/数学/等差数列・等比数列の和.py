c306080d29c127a668cc147c7dfc8d4aa9d11870
strangerxxxx <ogawa_first@yahoo.co.jp>
2022-08-24 14:14:30 +0900

    Update 等差数列・等比数列の和.py

--- python/数学/等差数列・等比数列の和.py
+++ python/数学/等差数列・等比数列の和.py
@@ -8,6 +8,11 @@ def arithmetic_progression_sum(a: int, d: int, n: int) -> int:
     return n * (2 * a + (n - 1) * d) // 2
 
 
+def arithmetic_progression_sum_2(a: int, l: int, n: int) -> int:
+    # 等差数列の和、初項a、末項l、項数n
+    return n * (a + l) // 2
+
+
 def geometric_progression_sum_mod(a: int, r: int, n: int, mod: int = 10 ** 9 + 7) -> int:
     # 等比数列の和、初項a、公比r、項数n
     if n <= 0:
@@ -24,3 +29,8 @@ def geometric_progression_sum_mod(a: int, r: int, n: int, mod: int = 10 ** 9 + 7
 def arithmetic_progression_sum_mod(a: int, d: int, n: int, mod: int = 10 ** 9 + 7) -> int:
     # 等差数列の和、初項a、公差d、項数n
     return (n * (2 * a + (n - 1) * d) // 2) % mod
+
+
+def arithmetic_progression_sum_mod_2(a: int, l: int, n: int, mod: int = 10 ** 9 + 7) -> int:
+    # 等差数列の和、初項a、末項l、項数n
+    return n * (a + l) // 2 % mod
