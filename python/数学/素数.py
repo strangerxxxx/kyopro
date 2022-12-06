17f33fd0a9f2c9af6988dd10c44797d01891e872
strangerxxxx <ogawa_first@yahoo.co.jp>
2022-12-06 10:29:29 +0900

    Update 素数.py

--- python/数学/素数.py
+++ python/数学/素数.py
@@ -1,53 +1,60 @@
-def faster_eratosthenes(n: int) -> list:
+def faster_eratosthenes(n: int):
     """
     ref : https://qiita.com/peria/items/54499b9ce9d5c1e93e5a
+    verify: https://judge.yosupo.jp/submission/110267
     """
     if n < 30:
         return [x for x in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] if x <= n]
     remains = [1, 7, 11, 13, 17, 19, 23, 29]
     inv_remains = {x: i for i, x in enumerate(remains)}
     msk = 255  # (1 << 8) - 1
-    div30 = [[i * j // 30 for j in remains] for i in remains]
-    mod30 = [[inv_remains[i * j % 30] for j in remains] for i in remains]
+    div30 = [i * j // 30 for j in remains for i in remains]
+    mod30 = [inv_remains[i * j % 30] for j in remains for i in remains]
     shift = [1 << i for i in range(8)]
     msk8 = [msk - shift[i] for i in range(8)]
     inv_shift = {shift[i]: i for i in range(8)}
     res = [2, 3, 5]
     max_k = n // 30
     import bisect
+
     max_m = bisect.bisect_right(remains, n % 30) - 1
-    sqrtn = int(n ** 0.5) + 1
+    sqrtn = int(n**0.5) + 1
     max_sqrt_k = sqrtn // 30
     max_sqrt_m = bisect.bisect_right(remains, sqrtn % 30) - 1
-    table = [msk] * (max_k + 1)
+    table = bytearray([msk] * (max_k + 1))
     table[max_k] = (1 << (max_m + 1)) - 1
     table[0] -= 1  # remove 1
     for k in range(max_sqrt_k + 1):
         for m in range(8):
             if k == max_sqrt_k and m > max_sqrt_m:
                 break
-            # k_before = k
-            m_before = m
             if table[k] & shift[m]:
-                i = k * (30 * k + 2 * remains[m]) + div30[m][m]
-                j = mod30[m][m]
+                # k_before = k
+                m_before = m
+                i = k * (30 * k + 2 * remains[m]) + div30[(m << 3) + m]
+                j = mod30[(m << 3) + m]
                 while i < max_k or (i == max_k and j <= max_m):
                     table[i] &= msk8[j]
                     if m_before == 7:
-                        i += 2 * k + remains[m] + div30[m][0] - div30[m][7]
-                        j = mod30[m][0]
+                        i += 2 * k + remains[m] + div30[m << 3] - div30[(m << 3) + 7]
+                        j = mod30[m << 3]
                         # k_before += 1
                         m_before = 0
                     else:
-                        i += k * (remains[m_before + 1] - remains[m_before]) + \
-                            div30[m][m_before + 1] - div30[m][m_before]
-                        j = mod30[m][m_before + 1]
+                        i += (
+                            k * (remains[m_before + 1] - remains[m_before])
+                            + div30[(m << 3) + m_before + 1]
+                            - div30[(m << 3) + m_before]
+                        )
+                        j = mod30[(m << 3) + m_before + 1]
                         m_before += 1
-    for i in range(max_k + 1):
-        while table[i]:
-            j = inv_shift[table[i] & (-table[i])]
-            res.append(i * 30 + remains[j])
-            table[i] &= table[i] - 1
+    i30 = 0
+    for i in table:
+        while i:
+            j = inv_shift[i & (-i)]
+            res.append(i30 + remains[j])
+            i &= i - 1
+        i30 += 30
     return res
 
 
@@ -56,53 +63,62 @@ def faster_eratosthenes_gen(n: int):
     ref : https://qiita.com/peria/items/54499b9ce9d5c1e93e5a
     """
     if n < 30:
-        return [x for x in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] if x <= n]
+        for i in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]:
+            if i > n:
+                break
+            yield i
+        return
     remains = [1, 7, 11, 13, 17, 19, 23, 29]
     inv_remains = {x: i for i, x in enumerate(remains)}
     msk = 255  # (1 << 8) - 1
-    div30 = [[i * j // 30 for j in remains] for i in remains]
-    mod30 = [[inv_remains[i * j % 30] for j in remains] for i in remains]
+    div30 = [i * j // 30 for j in remains for i in remains]
+    mod30 = [inv_remains[i * j % 30] for j in remains for i in remains]
     shift = [1 << i for i in range(8)]
     msk8 = [msk - shift[i] for i in range(8)]
     inv_shift = {shift[i]: i for i in range(8)}
-    yield 2
-    yield 3
-    yield 5
+    for i in (2, 3, 5):
+        yield i
     max_k = n // 30
     import bisect
+
     max_m = bisect.bisect_right(remains, n % 30) - 1
-    sqrtn = int(n ** 0.5) + 1
+    sqrtn = int(n**0.5) + 1
     max_sqrt_k = sqrtn // 30
     max_sqrt_m = bisect.bisect_right(remains, sqrtn % 30) - 1
-    table = [msk] * (max_k + 1)
+    table = bytearray([msk] * (max_k + 1))
     table[max_k] = (1 << (max_m + 1)) - 1
     table[0] -= 1  # remove 1
     for k in range(max_sqrt_k + 1):
         for m in range(8):
             if k == max_sqrt_k and m > max_sqrt_m:
                 break
-            # k_before = k
-            m_before = m
             if table[k] & shift[m]:
-                i = k * (30 * k + 2 * remains[m]) + div30[m][m]
-                j = mod30[m][m]
+                # k_before = k
+                m_before = m
+                i = k * (30 * k + 2 * remains[m]) + div30[(m << 3) + m]
+                j = mod30[(m << 3) + m]
                 while i < max_k or (i == max_k and j <= max_m):
                     table[i] &= msk8[j]
                     if m_before == 7:
-                        i += 2 * k + remains[m] + div30[m][0] - div30[m][7]
-                        j = mod30[m][0]
+                        i += 2 * k + remains[m] + div30[m << 3] - div30[(m << 3) + 7]
+                        j = mod30[m << 3]
                         # k_before += 1
                         m_before = 0
                     else:
-                        i += k * (remains[m_before + 1] - remains[m_before]) + \
-                            div30[m][m_before + 1] - div30[m][m_before]
-                        j = mod30[m][m_before + 1]
+                        i += (
+                            k * (remains[m_before + 1] - remains[m_before])
+                            + div30[(m << 3) + m_before + 1]
+                            - div30[(m << 3) + m_before]
+                        )
+                        j = mod30[(m << 3) + m_before + 1]
                         m_before += 1
-    for i in range(max_k + 1):
-        while table[i]:
-            j = inv_shift[table[i] & (-table[i])]
-            yield i * 30 + remains[j]
-            table[i] &= table[i] - 1
+    i30 = 0
+    for i in table:
+        while i:
+            j = inv_shift[i & (-i)]
+            yield i30 + remains[j]
+            i &= i - 1
+        i30 += 30
 
 
 def eratosthenes(limit: int, minLimit: int = 0) -> list:
@@ -145,7 +161,7 @@ def isPrime(n: int) -> bool:
     # nの素数判定
     if n < 2:
         return False
-    for i in range(2, int(n ** 0.5) + 1):
+    for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
     return True
@@ -154,7 +170,7 @@ def isPrime(n: int) -> bool:
 def count_primes(n):
     if n < 2:
         return 0
-    v = int(n ** 0.5) + 1
+    v = int(n**0.5) + 1
     smalls = [i // 2 for i in range(1, v + 1)]
     smalls[1] = 0
     s = v // 2
@@ -181,8 +197,9 @@ def count_primes(n):
             if skip[i]:
                 continue
             d = i * p
-            larges[ns] = larges[k] - \
-                (larges[smalls[d] - pc] if d < v else smalls[n // d]) + pc
+            larges[ns] = (
+                larges[k] - (larges[smalls[d] - pc] if d < v else smalls[n // d]) + pc
+            )
             roughs[ns] = i
             ns += 1
         s = ns
@@ -208,6 +225,7 @@ def count_primes(n):
 def miller_rabin(n: int, k: int = 100) -> bool:
     # ミラー–ラビン素数判定法
     import random
+
     if n == 2:
         return True
     if n == 1 or n & 1 == 0:
