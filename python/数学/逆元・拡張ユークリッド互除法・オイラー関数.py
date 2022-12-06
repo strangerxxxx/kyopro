797dc236e09036cfd556480ed1e568c018a75c1b
strangerxxxx <ogawa_first@yahoo.co.jp>
2022-12-06 10:26:39 +0900

    Update 逆元・拡張ユークリッド互除法・オイラー関数.py

--- python/数学/逆元・拡張ユークリッド互除法・オイラー関数.py
+++ python/数学/逆元・拡張ユークリッド互除法・オイラー関数.py
@@ -1,10 +1,11 @@
 def modinv(a: int, p: int = 1, m: int = 998244353) -> int:
-    # 逆元 a^(-p) == a^(m-1-p) mod m
+    # 逆元 a^(-p) == a^(m-1-p) mod m : mは素数
     return pow(a, m - 1 - p, m)
 
 
 def extgcd(a: int, b: int) -> int:
-    # 拡張ユークリッド互除法 gcd(a,b) と ax + by = gcd(a,b) の最小整数解
+    # 拡張ユークリッド互除法 ax + by = gcd(a,b) の最小整数解
+    # return gcd(a,b),a,b
     if b:
         d, y, x = extgcd(b, a % b)
         y -= (a // b) * x
@@ -13,22 +14,43 @@ def extgcd(a: int, b: int) -> int:
         return a, 1, 0
 
 
+def inv(a, m):
+    # aのmod m逆元 : mは非素数でも可
+    d, res, _ = extgcd(a, m)
+    if d != 1:
+        return None
+    return res
+
+
+def inv_calc(a, b, m):
+    # ax == b mod mとなる最小のx : mは非素数でも可
+    import math, functools
+
+    d = functools.reduce(math.gcd, (a, b, m))
+    a, b, m = a // d, b // d, m // d
+    x = inv(a, m)
+    if x is None:
+        return None
+    return b * x % m
+
+
 def euler_phi(n: int) -> int:
     # オイラー関数 φ(n) : 1,2,...,n-1のうちnと素であるものの数
+    # a^φ(n)==1 mod n (aとnが素なとき)
     res = n
-    for x in range(2, int(n ** 0.5) + 1):
+    for x in range(2, int(n**0.5) + 1):
         if n % x == 0:
             res = res // x * (x - 1)
             while n % x == 0:
                 n //= x
     if n > 1:
-        res = res // n * (n-1)
+        res = res // n * (n - 1)
     return res
 
 
 def euler_list(n: int):
     # φ(x) for 0 <= x <= M
-    *phi, = range(n + 1)
+    (*phi,) = range(n + 1)
     for x in range(2, n + 1):
         if phi[x] == x:
             for y in range(x, n + 1, x):
