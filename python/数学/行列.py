8994a78ea62fe7432484b6ff39f8729f9ee8ded1
strangerxxxx <ogawa_first@yahoo.co.jp>
2022-12-14 11:09:55 +0900

    Update 行列.py

--- python/数学/行列.py
+++ python/数学/行列.py
@@ -1,203 +1,3 @@
-MOD = 998244353
-
-
-class Matrix():
-    def __init__(self, n, m, mat=None):
-        self.n = n
-        self.m = m
-        self.mat = [[0] * self.m for _ in range(self.n)]
-        if mat:
-            # assert len(mat) == n and len(mat[0]) == m
-            for i in range(self.n):
-                self.mat[i] = mat[i].copy()
-
-    def is_square(self):
-        return self.n == self.m
-
-    def determinant(self):
-        # assert self.is_square()
-        res = 1
-        tmp = Matrix(self.n, self.n, self.mat)
-        for j in range(self.n):
-            if tmp[j][j] == 0:
-                for i in range(j + 1, self.n):
-                    if tmp[i][j]:
-                        break
-                else:
-                    return 0
-                tmp.mat[j], tmp.mat[i] = tmp.mat[i], tmp.mat[j]
-                res *= -1
-            tmp_j = tmp[j]
-            inv = pow(tmp_j[j], MOD - 2, MOD)
-            for i in range(j + 1, self.n):
-                tmp_i = tmp[i]
-                c = -inv * tmp_i[j] % MOD
-                for k in range(self.n):
-                    tmp_i[k] += c * tmp_j[k]
-                    tmp_i[k] %= MOD
-        for i in range(self.n):
-            res *= tmp[i][i]
-            res %= MOD
-        return res
-
-    def inverse(self):
-        # assert self.is_square()
-        res = Matrix.id(self.n)
-        tmp = Matrix(self.n, self.n, self.mat)
-        for j in range(self.n):
-            if tmp[j][j] == 0:
-                for i in range(j + 1, self.n):
-                    if tmp[i][j]:
-                        break
-                else:
-                    return -1
-                tmp.mat[j], tmp.mat[i] = tmp.mat[i], tmp.mat[j]
-                res.mat[j], res.mat[i] = res.mat[i], res.mat[j]
-            tmp_j, res_j = tmp[j], res[j]
-            inv = pow(tmp_j[j], MOD - 2, MOD)
-            for k in range(self.n):
-                tmp_j[k] *= inv
-                tmp_j[k] %= MOD
-                res_j[k] *= inv
-                res_j[k] %= MOD
-            for i in range(self.n):
-                if i == j:
-                    continue
-                c = tmp[i][j]
-                tmp_i, res_i = tmp[i], res[i]
-                for k in range(self.n):
-                    tmp_i[k] -= tmp_j[k] * c
-                    tmp_i[k] %= MOD
-                    res_i[k] -= res_j[k] * c
-                    res_i[k] %= MOD
-        return res
-
-    def linear_equations(self, vec):
-        # assert self.n == len(vec)
-        aug = [self[i] + [vec[i]] for i in range(self.n)]
-        rank = 0
-        p = []
-        q = []
-        for j in range(self.m + 1):
-            for i in range(rank, self.n):
-                if aug[i][j]:
-                    break
-            else:
-                q.append(j)
-                continue
-            if j == self.m:
-                return -1, [], []
-            p.append(j)
-            aug[rank], aug[i] = aug[i], aug[rank]
-            inv = pow(aug[rank][j], MOD - 2, MOD)
-            aug_rank = aug[rank]
-            for k in range(self.m + 1):
-                aug_rank[k] *= inv
-                aug_rank[k] %= MOD
-            for i in range(self.n):
-                if i == rank:
-                    continue
-                aug_i = aug[i]
-                c = -aug_i[j]
-                for k in range(self.m + 1):
-                    aug_i[k] += c * aug_rank[k]
-                    aug_i[k] %= MOD
-            rank += 1
-        dim = self.m - rank
-        sol = [0] * self.m
-        for i in range(rank):
-            sol[p[i]] = aug[i][-1]
-        vecs = [[0] * self.m for _ in range(dim)]
-        for i in range(dim):
-            vecs[i][q[i]] = 1
-        for i in range(dim):
-            vecs_i = vecs[i]
-            for j in range(rank):
-                vecs_i[p[j]] = -aug[j][q[i]] % MOD
-        return dim, sol, vecs
-
-    def __getitem__(self, key):
-        if not isinstance(key, slice):
-            assert key >= 0
-        return self.mat[key]
-
-    def id(n):
-        res = Matrix(n, n)
-        for i in range(n):
-            res[i][i] = 1
-        return res
-
-    def __len__(self):
-        return len(self.mat)
-
-    def __str__(self):
-        return '\n'.join(' '.join(map(str, self[i])) for i in range(self.n))
-
-    def times(self, k):
-        res = [[0] * self.m for _ in range(self.n)]
-        for i in range(self.n):
-            res_i, self_i = res[i], self[i]
-            for j in range(self.m):
-                res_i[j] = k * self_i[j] % MOD
-        return Matrix(self.n, self.m, res)
-
-    def __pos__(self):
-        return self
-
-    def __neg__(self):
-        return self.times(-1)
-
-    def __add__(self, other):
-        # assert self.n == other.n and self.m == other.m
-        res = [[0] * self.m for _ in range(self.n)]
-        for i in range(self.n):
-            res_i, self_i, other_i = res[i], self[i], other[i]
-            for j in range(self.m):
-                res_i[j] = (self_i[j] + other_i[j]) % MOD
-        return Matrix(self.n, self.m, res)
-
-    def __sub__(self, other):
-        # assert self.n == other.n and self.m == other.m
-        res = [[0] * self.m for _ in range(self.n)]
-        for i in range(self.n):
-            res_i, self_i, other_i = res[i], self[i], other[i]
-            for j in range(self.m):
-                res_i[j] = (self_i[j] - other_i[j]) % MOD
-        return Matrix(self.n, self.m, res)
-
-    def __mul__(self, other):
-        if other.__class__ == Matrix:
-            # assert self.m == other.n
-            res = [[0] * other.m for _ in range(self.n)]
-            for i in range(self.n):
-                res_i, self_i = res[i], self[i]
-                for k in range(self.m):
-                    self_ik, other_k = self_i[k], other[k]
-                    for j in range(other.m):
-                        res_i[j] += self_ik * other_k[j]
-                        res_i[j] %= MOD
-            return Matrix(self.n, other.m, res)
-        else:
-            return self.times(other)
-
-    def __rmul__(self, other):
-        return self.times(other)
-
-    def __pow__(self, k):
-        # assert self.is_square()
-        tmp = Matrix(self.n, self.n, self.mat)
-        res = Matrix.id(self.n)
-        i = abs(k)
-        while i:
-            if i & 1:
-                res *= tmp
-            tmp *= tmp
-            i >>= 1
-        if k < 0:
-            res = res.inverse()
-        return res
-
-
 def transpose(x):
     return [y for y in zip(*x)]
 
@@ -281,6 +81,7 @@ def matIndex(x, s):
 def determinant(A, replace=False, mod=998244353):
     if not replace:
         from copy import deepcopy
+
         A = deepcopy(A)
     n = len(A)
     res = 1
@@ -307,13 +108,212 @@ def determinant(A, replace=False, mod=998244353):
     return res
 
 
+class Matrix:
+    MOD = 998244353
+
+    def __init__(self, n, m, mat=None):
+        self.n = n
+        self.m = m
+        self.mat = [[0] * self.m for _ in range(self.n)]
+        if mat:
+            # assert len(mat) == n and len(mat[0]) == m
+            for i in range(self.n):
+                self.mat[i] = mat[i].copy()
+
+    def is_square(self):
+        return self.n == self.m
+
+    def determinant(self):
+        # assert self.is_square()
+        res = 1
+        tmp = Matrix(self.n, self.n, self.mat)
+        for j in range(self.n):
+            if tmp[j][j] == 0:
+                for i in range(j + 1, self.n):
+                    if tmp[i][j]:
+                        break
+                else:
+                    return 0
+                tmp.mat[j], tmp.mat[i] = tmp.mat[i], tmp.mat[j]
+                res *= -1
+            tmp_j = tmp[j]
+            inv = pow(tmp_j[j], self.MOD - 2, self.MOD)
+            for i in range(j + 1, self.n):
+                tmp_i = tmp[i]
+                c = -inv * tmp_i[j] % self.MOD
+                for k in range(self.n):
+                    tmp_i[k] += c * tmp_j[k]
+                    tmp_i[k] %= self.MOD
+        for i in range(self.n):
+            res *= tmp[i][i]
+            res %= self.MOD
+        return res
+
+    def inverse(self):
+        # assert self.is_square()
+        res = Matrix.id(self.n)
+        tmp = Matrix(self.n, self.n, self.mat)
+        for j in range(self.n):
+            if tmp[j][j] == 0:
+                for i in range(j + 1, self.n):
+                    if tmp[i][j]:
+                        break
+                else:
+                    return -1
+                tmp.mat[j], tmp.mat[i] = tmp.mat[i], tmp.mat[j]
+                res.mat[j], res.mat[i] = res.mat[i], res.mat[j]
+            tmp_j, res_j = tmp[j], res[j]
+            inv = pow(tmp_j[j], self.MOD - 2, self.MOD)
+            for k in range(self.n):
+                tmp_j[k] *= inv
+                tmp_j[k] %= self.MOD
+                res_j[k] *= inv
+                res_j[k] %= self.MOD
+            for i in range(self.n):
+                if i == j:
+                    continue
+                c = tmp[i][j]
+                tmp_i, res_i = tmp[i], res[i]
+                for k in range(self.n):
+                    tmp_i[k] -= tmp_j[k] * c
+                    tmp_i[k] %= self.MOD
+                    res_i[k] -= res_j[k] * c
+                    res_i[k] %= self.MOD
+        return res
+
+    def linear_equations(self, vec):
+        # assert self.n == len(vec)
+        aug = [self[i] + [vec[i]] for i in range(self.n)]
+        rank = 0
+        p = []
+        q = []
+        for j in range(self.m + 1):
+            for i in range(rank, self.n):
+                if aug[i][j]:
+                    break
+            else:
+                q.append(j)
+                continue
+            if j == self.m:
+                return -1, [], []
+            p.append(j)
+            aug[rank], aug[i] = aug[i], aug[rank]
+            inv = pow(aug[rank][j], self.MOD - 2, self.MOD)
+            aug_rank = aug[rank]
+            for k in range(self.m + 1):
+                aug_rank[k] *= inv
+                aug_rank[k] %= self.MOD
+            for i in range(self.n):
+                if i == rank:
+                    continue
+                aug_i = aug[i]
+                c = -aug_i[j]
+                for k in range(self.m + 1):
+                    aug_i[k] += c * aug_rank[k]
+                    aug_i[k] %= self.MOD
+            rank += 1
+        dim = self.m - rank
+        sol = [0] * self.m
+        for i in range(rank):
+            sol[p[i]] = aug[i][-1]
+        vecs = [[0] * self.m for _ in range(dim)]
+        for i in range(dim):
+            vecs[i][q[i]] = 1
+        for i in range(dim):
+            vecs_i = vecs[i]
+            for j in range(rank):
+                vecs_i[p[j]] = -aug[j][q[i]] % self.MOD
+        return dim, sol, vecs
+
+    def __getitem__(self, key):
+        if not isinstance(key, slice):
+            assert key >= 0
+        return self.mat[key]
+
+    def id(n):
+        res = Matrix(n, n)
+        for i in range(n):
+            res[i][i] = 1
+        return res
+
+    def __len__(self):
+        return len(self.mat)
+
+    def __str__(self):
+        return "\n".join(" ".join(map(str, self[i])) for i in range(self.n))
+
+    def times(self, k):
+        res = [[0] * self.m for _ in range(self.n)]
+        for i in range(self.n):
+            res_i, self_i = res[i], self[i]
+            for j in range(self.m):
+                res_i[j] = k * self_i[j] % self.MOD
+        return Matrix(self.n, self.m, res)
+
+    def __pos__(self):
+        return self
+
+    def __neg__(self):
+        return self.times(-1)
+
+    def __add__(self, other):
+        # assert self.n == other.n and self.m == other.m
+        res = [[0] * self.m for _ in range(self.n)]
+        for i in range(self.n):
+            res_i, self_i, other_i = res[i], self[i], other[i]
+            for j in range(self.m):
+                res_i[j] = (self_i[j] + other_i[j]) % self.MOD
+        return Matrix(self.n, self.m, res)
+
+    def __sub__(self, other):
+        # assert self.n == other.n and self.m == other.m
+        res = [[0] * self.m for _ in range(self.n)]
+        for i in range(self.n):
+            res_i, self_i, other_i = res[i], self[i], other[i]
+            for j in range(self.m):
+                res_i[j] = (self_i[j] - other_i[j]) % self.MOD
+        return Matrix(self.n, self.m, res)
+
+    def __mul__(self, other):
+        if other.__class__ == Matrix:
+            # assert self.m == other.n
+            res = [[0] * other.m for _ in range(self.n)]
+            for i in range(self.n):
+                res_i, self_i = res[i], self[i]
+                for k in range(self.m):
+                    self_ik, other_k = self_i[k], other[k]
+                    for j in range(other.m):
+                        res_i[j] += self_ik * other_k[j]
+                        res_i[j] %= self.MOD
+            return Matrix(self.n, other.m, res)
+        else:
+            return self.times(other)
+
+    def __rmul__(self, other):
+        return self.times(other)
+
+    def __pow__(self, k):
+        # assert self.is_square()
+        tmp = Matrix(self.n, self.n, self.mat)
+        res = Matrix.id(self.n)
+        i = abs(k)
+        while i:
+            if i & 1:
+                res *= tmp
+            tmp *= tmp
+            i >>= 1
+        if k < 0:
+            res = res.inverse()
+        return res
+
+
 class BitMatrix:
-    __slots__ = ['n', 'mat', 'mask', 'u', 'v']
+    __slots__ = ["n", "mat", "mask", "u", "v"]
 
     def __init__(self, n):
         self.n = n
         self.mat = 0
-        self.u = u = 2 ** n - 1
+        self.u = u = 2**n - 1
         self.v = ((u + 1) ** n - 1) // u
 
     def copy(self, other):
