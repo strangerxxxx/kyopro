6ce843921f1dfda329fac5a21d26d8a00462766f
strangerxxxx <ogawa_first@yahoo.co.jp>
2022-12-16 11:29:22 +0900

    Update 木の深さ.py

--- python/木/木の深さ.py
+++ python/木/木の深さ.py
@@ -1,9 +1,6 @@
-def treeDepth(n: int, edge, start: int = 0, need_path: bool = False) -> int:
+def treeDepth(n: int, edges, start: int = 0, need_path: bool = False):
+    # 木の深さ(重みなし)
     from collections import deque
-    childs = [[] for _ in range(n)]
-    for i, j in edge:
-        childs[i - 1].append(j - 1)
-        childs[j - 1].append(i - 1)
 
     def bfs(start: int, need_path: bool = False):
         q = deque()
@@ -14,7 +11,7 @@ def treeDepth(n: int, edge, start: int = 0, need_path: bool = False) -> int:
         while q:
             p, distance = q.popleft()
             last = p
-            for i in childs[p]:
+            for i in edges[p]:
                 if before[i] is None:
                     q.append((i, distance + 1))
                     before[i] = p
@@ -26,6 +23,11 @@ def treeDepth(n: int, edge, start: int = 0, need_path: bool = False) -> int:
             p = before[p]
             path.append(p)
         return last, distance, path[::-1]
+
+    if n == 1:
+        if need_path:
+            return 0, [0]
+        return 0
     p1, _ = bfs(start, False)
     if need_path:
         _, d2, path = bfs(p1, True)
@@ -34,24 +36,20 @@ def treeDepth(n: int, edge, start: int = 0, need_path: bool = False) -> int:
     return d2
 
 
-def treeHeight(n: int, edge, start: int = None) -> int:
-    childs = [[] for _ in range(n)]
-    for i, j, d in edge:
-        childs[i - 1].append((j - 1, d))
-        childs[j - 1].append((i - 1, d))
-
+def treeHeight(n: int, edges, start: int = None) -> int:
+    # 木の深さ(重みあり)
+    # verified: https://bit.ly/3qqtlGI
     def bfs(start: int = 0, need_path: bool = False):
         q = []
-        last = start
         before = [None] * n
         q.append((0, start, -1))
-        farp, fard = None, 0
+        farp, fard = start, 0
         while q:
             distance, p, pos = q.pop()
             before[p] = pos
             if distance > fard:
                 farp, fard = p, distance
-            for i, dist in childs[p]:
+            for i, dist in edges[p]:
                 if i == pos:
                     continue
                 q.append((distance + dist, i, p))
@@ -63,6 +61,7 @@ def treeHeight(n: int, edge, start: int = None) -> int:
             p = before[p]
             path.append(p)
         return farp, fard, path[::-1]
+
     if start is None:
         start, _ = bfs(0)
     _, d = bfs(start)
