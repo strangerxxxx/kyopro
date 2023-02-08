07e7439789d6decd9dc514a6ae96741469efbdbf
strangerxxxx <ogawa_first@yahoo.co.jp>
2023-02-08 11:03:24 +0900

    Update 偏角ソート.py

--- python/図形/偏角ソート.py
+++ python/図形/偏角ソート.py
@@ -21,8 +21,8 @@ def declinination_sort(l):
                 orthant = 3
                 a, b = -x, -y
         # 象限と優先度の変換
-        d = {0: 3, 1: 2, 2: 1, 3: 5, 4: 4}  # -pi<=theta<pi のとき
-        # d = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4}  # 0<=theta<2pi のときはこっち
+        d = [3, 2, 1, 5, 4]  # -pi<=theta<pi のとき
+        # d = [0, 1, 2, 3, 4]  # 0<=theta<2pi のときはこっち
         return a, b, d[orthant]
 
     def arg_sort(p0, p1):
