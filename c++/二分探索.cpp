95488ae0c57bcfab7800b19a283e530b4ba8b36e
strangerxxxx <ogawa_first@yahoo.co.jp>
2023-03-17 13:03:45 +0900

    Update 二分探索.cpp

--- c++/二分探索.cpp
+++ c++/二分探索.cpp
@@ -1,22 +1,19 @@
-#ifdef STRANGERXXX
-#define _GLIBCXX_DEBUG
-#endif
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-bool f(ll mid)
-{
-  return true;
-}
 int main()
 {
   ll ok = 0;
   ll ng = 1E18;
   ll mid;
+  auto check = [&](ll mid) -> bool
+  {
+    return true;
+  };
   while (abs(ok - ng) > 1)
   {
     mid = (ok + ng) / 2;
-    if (f(mid))
+    if (check(mid))
     {
       ok = mid;
     }
