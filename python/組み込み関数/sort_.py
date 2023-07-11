8521b17a7517ea7af1c803315683eaec7ea4b654
strangerxxxx <ogawa_first@yahoo.co.jp>
2023-07-11 21:36:19 +0900

    Update sort_.py

--- python/組み込み関数/sort_.py
+++ python/組み込み関数/sort_.py
@@ -13,3 +13,26 @@ def bucket_sort(a, i_min: int = None, i_max: int = None):
             res[i] = j + i_min
             i += 1
     return res
+
+
+class HilbertOrder:
+    def __init__(self, n: int) -> None:
+        # n : x, yの最大値
+        self.maxn = 1 << (n - 1).bit_length()
+
+    def __call__(self, x: int, y: int):
+        d = 0
+        s = self.maxn >> 1
+        while s:
+            rx = (x & s) > 0
+            ry = (y & s) > 0
+            d += s**2 * ((rx * 3) ^ ry)
+            if ry:
+                s >>= 1
+                continue
+            if rx:
+                x = self.maxn - 1 - x
+                y = self.maxn - 1 - y
+            x, y = y, x
+            s >>= 1
+        return d
