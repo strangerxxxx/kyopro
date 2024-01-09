20da41a40acc59a49497efb9607c510ba84291d3
strangerxxxx <ogawa_first@yahoo.co.jp>
2024-01-09 12:16:17 +0900

    Update 偏角ソート.py

--- python/図形/偏角ソート.py
+++ python/図形/偏角ソート.py
@@ -1,38 +1,26 @@
 def declinination_sort(l):
     from functools import cmp_to_key
 
-    def convert_arg(p):
-        x, y = p
-        if x >= 0:
-            if y > 0:
-                orthant = 1
-                a, b = x, y
-            elif y < 0:
-                orthant = 4
-                a, b = -y, x
-            else:
-                orthant = 0
-                a, b = x, y
+    upper = []
+    zero = []
+    under = []
+    for i in l:
+        if i[1] > 0:
+            upper.append(i)
+        elif i[1] < 0:
+            under.append(i)
+        elif i[0] > 0:
+            under.append(i)
+        elif i[0] < 0:
+            upper.append(i)
         else:
-            if y >= 0:
-                orthant = 2
-                a, b = y, -x
-            else:
-                orthant = 3
-                a, b = -x, -y
-        # 象限と優先度の変換
-        d = [3, 2, 1, 5, 4]  # -pi<=theta<pi のとき
-        # d = [0, 1, 2, 3, 4]  # 0<=theta<2pi のときはこっち
-        return a, b, d[orthant]
+            zero.append(i)
 
-    def arg_sort(p0, p1):
-        x0, y0, orthant0 = convert_arg(p0)
-        x1, y1, orthant1 = convert_arg(p1)
-        if orthant0 < orthant1:
-            return 1
-        if orthant0 > orthant1:
-            return -1
-        if x0 * y1 == x1 * y0:
-            return 0
-        return 1 if x0 * y1 < x1 * y0 else -1
-    return sorted(l, key=cmp_to_key(arg_sort))
+    def arg_sort(x, y):
+        return x[1] * y[0] - x[0] * y[1]
+
+    return (
+        sorted(under, key=cmp_to_key(arg_sort))
+        + zero
+        + sorted(upper, key=cmp_to_key(arg_sort))
+    )
