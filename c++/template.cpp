4a8d073b615bc8c70bd51a9629b3547acf3cd3fd
strangerxxxx <ogawa_first@yahoo.co.jp>
2024-01-19 12:58:05 +0900

    Update template.cpp

--- c++/template.cpp
+++ c++/template.cpp
@@ -3,18 +3,28 @@
 #ifdef STRANGERXXX
 #define _GLIBCXX_DEBUG
 #endif
-// #pragma GCC target("avx2")
+#ifdef ATCODER
+#pragma GCC target("arch=skylake-avx512")
+#endif
 #pragma GCC optimize("O3")
 #pragma GCC optimize("unroll-loops")
 // #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 #include <bits/stdc++.h>
 using namespace std;
+#if __has_include(<atcoder/all>)
 #include <atcoder/all>
 using namespace atcoder;
 using mint = modint998244353;
 // using mint = modint1000000007;
-// #include <boost/multiprecision/cpp_int.hpp>
-// using namespace boost::multiprecision;
+ostream &operator<<(ostream &os, const mint &i) {
+    os << i.val();
+    return os;
+}
+#endif
+#if __has_include(<boost/multiprecision/cpp_int.hpp>)
+#include <boost/multiprecision/cpp_int.hpp>
+using namespace boost::multiprecision;
+#endif
 typedef long long ll;
 constexpr ll MOD = 998244353;
 typedef unsigned int uint;
@@ -33,24 +43,21 @@ constexpr int dx[4] = {1, 0, -1, 0};
 constexpr int dy[4] = {0, 1, 0, -1};
 #if __cplusplus >= 201707L
 #define REP(i, end) for (int i : views::iota(0, (end)))
+#define REP_(end) for ([[maybe_unused]] int _ : views::iota(0, (end)))
 #define REP3(i, begin, end) for (auto i : views::iota((begin), (end)))
-#define REP4(i, begin, end, step)                                \
-    for (int i :                                                 \
-         views::iota(0, ((end) - (begin) + (step)-1) / (step)) | \
-             views::transform([](auto x) { return x * (step) + (begin); }))
 #define REPR(i, end) for (int i : views::iota(0, (end)) | views::reverse)
 #define REP3R(i, begin, end) \
     for (auto i : views::iota((begin), (end)) | views::reverse)
-#define REP4R(i, begin, end, step)                                       \
-    for (int i : views::iota(0, ((end) - (begin) + (step)-1) / (step)) | \
-                     views::reverse | views::transform([](ll x) {        \
-                         return x * (step) + (begin);                    \
-                     }))
 #define REPAR(i, I) for (const auto &i : I | views::reverse)
 #else
 #define REP(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define REP_(n) for (ll _ = 0; _ < (ll)(n); _++)
 #define REP3(i, m, n) for (ll i = (m); i < (ll)(n); i++)
 #endif
+#define REP4(i, begin, end, step) \
+    for (ll i = (begin); i < (ll)(end); i = i + (ll)(step))
+#define REP4R(i, begin, end, step) \
+    for (ll i = (begin); i >= (ll)(end); i = i + (ll)(step))
 #define REPA(i, I) for (const auto &i : I)
 #define REPB(i, I) for (auto &&i : I)
 #define REPIJ(i, j, n)               \
@@ -60,6 +67,20 @@ constexpr int dy[4] = {0, 1, 0, -1};
     for (auto &&i : a) { \
         cin >> i;        \
     }
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
 #define LEN(x) ssize(x)
 typedef pair<ll, ll> PII;
 typedef vector<ll> VI;
@@ -70,9 +91,9 @@ typedef vector<VS> VVS;
 typedef vector<PII> VP;
 typedef set<ll> SI;
 typedef multiset<ll> MSI;
+typedef unordered_set<ll, custom_hash> USI;
 typedef map<ll, ll> MI;
-typedef unordered_set<ll> USI;
-typedef unordered_map<ll, ll> UMI;
+typedef unordered_map<ll, ll, custom_hash> UMI;
 typedef priority_queue<ll> PQ;
 typedef priority_queue<ll, VI, greater<ll>> RPQ;
 typedef deque<ll> DQ;
@@ -88,11 +109,11 @@ mt19937_64 mt64(seed_gen());
 #define endl '\n'
 #define ALL(V) (V).begin(), (V).end()
 #define SUM(V) reduce((V).begin(), (V).end(), 0LL)
+#if __cplusplus >= 201707L
 #define SORT(V) ranges::sort(V)
 #define RSORT(V) ranges::sort(V, ranges::greater());
 #define REVERSE(V) ranges::reverse(V)
 #define MINMAX(a, b) ranges::minmax(a, b)
-#define mod(a, b) ((a) % (b) + (b)) % (b)
 #define MAXVI(V) *ranges::max_element(V)
 #define MINVI(V) *ranges::min_element(V)
 #define UB(V, x) ranges::upper_bound(V, x)
@@ -100,6 +121,9 @@ mt19937_64 mt64(seed_gen());
 #define bisect_left(V, x) ranges::lower_bound(V, x) - (V).begin()
 #define bisect_right(V, x) ranges::upper_bound(V, x) - (V).begin()
 #define BS(V, x) ranges::binary_search(V, x)
+#define IN(V, x) (V).contains(x)
+#endif
+#define mod(a, b) ((a) % (b) + (b)) % (b)
 #define MEMSET(v, h) memset((v), h, sizeof(v))
 #define MEMCPY(v, h) memcpy((v), h, sizeof(v))
 template <class T>
@@ -110,17 +134,17 @@ template <class T>
 bool chmax(T &a, const T &b) {
     if (a < b) {
         a = b;
-        return 1;
+        return true;
     }
-    return 0;
+    return false;
 }
 template <class T>
 bool chmin(T &a, const T &b) {
     if (b < a) {
         a = b;
-        return 1;
+        return true;
     }
-    return 0;
+    return false;
 }
 string join(const vector<string> &v, const char *delim = 0) {
     string s;
@@ -133,8 +157,63 @@ string join(const vector<string> &v, const char *delim = 0) {
     }
     return s;
 }
-ostream &operator<<(ostream &os, const mint &i) {
-    os << i.val();
+template <class Key, class Compare, class Allocator>
+ostream &operator<<(ostream &os, const set<Key, Compare, Allocator> &st) {
+    auto itr = st.begin();
+    for (int i = 0; i < (int)st.size(); i++) {
+        cout << (itr == st.begin() ? "" : " ") << *itr;
+        itr++;
+    }
+    return os;
+}
+template <class Key, class Hash, class Pred, class Allocator>
+ostream &operator<<(ostream &os, unordered_set<Key, Hash, Pred, Allocator> st) {
+    auto itr = st.begin();
+    for (int i = 0; i < (int)st.size(); i++) {
+        cout << (itr == st.begin() ? "" : " ") << *itr;
+        itr++;
+    }
+    return os;
+}
+template <class Key, class Compare, class Allocator>
+ostream &operator<<(ostream &os, multiset<Key, Compare, Allocator> st) {
+    auto itr = st.begin();
+    for (int i = 0; i < (int)st.size(); i++) {
+        cout << (itr == st.begin() ? "" : " ") << *itr;
+        itr++;
+    }
+    return os;
+}
+template <class Key, class T, class Compare, class Allocator>
+ostream &operator<<(ostream &os, const map<Key, T, Compare, Allocator> &mp) {
+    cout << '{';
+    for (auto &[key, val] : mp) {
+        cout << key << ": " << val << ", ";
+    }
+    cout << '}';
+    return os;
+}
+template <class Key, class T, class Hash, class Pred, class Allocator>
+ostream &operator<<(ostream &os,
+                    const unordered_map<Key, T, Hash, Pred, Allocator> &mp) {
+    cout << '{';
+    for (auto &[key, val] : mp) {
+        cout << key << ": " << val << ", ";
+    }
+    cout << '}';
+    return os;
+}
+template <class T, class Container, class Compare>
+ostream &operator<<(ostream &os, priority_queue<T, Container, Compare> pq) {
+    while (!pq.empty()) {
+        os << pq.top() << " ";
+        pq.pop();
+    }
+    return os;
+}
+template <class T1, class T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p) {
+    cout << p.first << " " << p.second;
     return os;
 }
 void print() { cout << endl; }
@@ -164,53 +243,6 @@ void print(const vector<vector<T>> &V) {
         print(i);
     }
 }
-template <typename T1, typename T2>
-void print(const pair<T1, T2> &P) {
-    cout << P.first << " " << P.second << endl;
-}
-template <typename T>
-void print(const set<T> &st) {
-    auto itr = st.begin();
-    for (int i = 0; i < (int)st.size(); i++) {
-        cout << (itr == st.begin() ? "" : " ") << *itr;
-        itr++;
-    }
-    cout << endl;
-}
-template <typename T>
-void print(const unordered_set<T> &st) {
-    auto itr = st.begin();
-    for (int i = 0; i < (int)st.size(); i++) {
-        cout << (itr == st.begin() ? "" : " ") << *itr;
-        itr++;
-    }
-    cout << endl;
-}
-template <typename T>
-void print(const multiset<T> &st) {
-    auto itr = st.begin();
-    for (int i = 0; i < (int)st.size(); i++) {
-        cout << (itr == st.begin() ? "" : " ") << *itr;
-        itr++;
-    }
-    cout << endl;
-}
-template <typename T1, typename T2>
-void print(const map<T1, T2> &mp) {
-    cout << '{';
-    for (auto &[key, val] : mp) {
-        cout << key << ": " << val << ", ";
-    }
-    cout << '}' << endl;
-}
-template <typename T1, typename T2>
-void print(const unordered_map<T1, T2> &mp) {
-    cout << '{';
-    for (auto &[key, val] : mp) {
-        cout << key << ": " << val << ", ";
-    }
-    cout << '}' << endl;
-}
 template <class T>
 vector<pair<ll, T>> enumerate(const vector<T> &a, ll start = 0, ll step = 1) {
     vector<pair<ll, T>> res;
@@ -221,21 +253,6 @@ vector<pair<ll, T>> enumerate(const vector<T> &a, ll start = 0, ll step = 1) {
     }
     return res;
 }
-struct custom_hash {
-    static uint64_t splitmix64(uint64_t x) {
-        // http://xorshift.di.unimi.it/splitmix64.c
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-
-    size_t operator()(uint64_t x) const {
-        static const uint64_t FIXED_RANDOM =
-            chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
 ll divceil(ll a, ll b) { return (a + (b - 1)) / b; }
 PII divmod(ll a, ll b) {
     ll m = mod(a, b);
