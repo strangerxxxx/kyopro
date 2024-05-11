8233434ab1ed05dbaaeb99cc4aa59907f9ffb122
strangerxxxx <ogawa_first@yahoo.co.jp>
2024-05-11 11:00:57 +0900

    Update 木の重心.py

--- python/木/木の重心.py
+++ python/木/木の重心.py
@@ -23,3 +23,31 @@ def center_of_tree(edges: list[list[int]], start: int = 0) -> int:
                 break
         else:
             return i
+
+
+def center_of_tree2(edges: list[list[int]], weight: list[int], start: int = 0) -> int:
+    n = len(edges)
+    s = sum(weight) // 2
+    child_size = weight[:]
+    parent = [-1] * n
+    order = []
+    q = [start]
+    while q:
+        i = q.pop()
+        order.append(i)
+        for j in edges[i]:
+            if j != parent[i]:
+                q.append(j)
+                parent[j] = i
+    for i in reversed(order):
+        for j in edges[i]:
+            if j != parent[i]:
+                child_size[i] += child_size[j]
+    i = start
+    while True:
+        for j in edges[i]:
+            if j != parent[i] and child_size[j] > s:
+                i = j
+                break
+        else:
+            return i
