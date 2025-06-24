2a6dfface69ce8312be93a0049e108b245ea6b06
stranger <dendeneden@gmail.com>
2025-06-24 18:51:13 +0900

    文字列結合アルゴリズムの改善

--- python/文字列/ランレングス圧縮.py
+++ python/文字列/ランレングス圧縮.py
@@ -1,26 +1,27 @@
-def runLengthEncode(S: str) -> "List[tuple(str, int)]":
+def runLengthEncode(s: str) -> list[tuple[str, int]]:
     from itertools import groupby
-    # RUN LENGTH ENCODING str -> list(tuple())
+
+    # RUN LENGTH ENCODING str -> list[tuple[str, int]]
     # example) "aabbbbaaca" -> [('a', 2), ('b', 4), ('a', 2), ('c', 1), ('a', 1)]
-    grouped = groupby(S)
+    grouped = groupby(s)
     res = []
     for k, v in grouped:
         res.append((k, int(len(list(v)))))
     return res
 
 
-def runLengthDecode(L: "list[tuple]") -> str:
-    from itertools import groupby
-    # RUN LENGTH DECODING list(tuple()) -> str
+def runLengthDecode(a: list[tuple]) -> str:
+    # RUN LENGTH DECODING list[tuple[str, int]] -> str
     # example) [('a', 2), ('b', 4), ('a', 2), ('c', 1), ('a', 1)] -> "aabbbbaaca"
-    res = ""
-    for c, n in L:
-        res += c * int(n)
-    return res
+    res = []
+    for c, n in a:
+        res.append(c * int(n))
+    return "".join(res)
 
 
 def runLengthEncodeToString(S: str) -> str:
     from itertools import groupby
+
     # RUN LENGTH ENCODING str -> str
     # example) "aabbbbaaca" -> "a2b4a2c1a1"
     grouped = groupby(S)
